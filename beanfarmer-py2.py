ec10db8d5572f1d5d6127f71e10a91fe77eaf8c8
Ewan Barr <ewan.d.barr@googlmail.com>
2017-06-12 11:51:00 +0200

    added python 2 support

--- beanfarmer-py2.py
+++ beanfarmer-py2.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 """
 Copyright (c) 2017 Ewan D. Barr
 
@@ -28,8 +27,8 @@ from subprocess import Popen,PIPE
 
 DEBUG = False
 VERBOSE = False
-SOURCE_FILE = "./ibeanfarmer_noshfl_k.cu"
-EXECUTABLE = "./ibeanfarmer_autogen"
+SOURCE_FILE = "./beanfarmer_dp4a_noshfl_k.cu"
+EXECUTABLE = "./beanfarmer_autogen"
 
 class Tee(object):
     def __init__(self,filename):
@@ -45,6 +44,7 @@ class Tee(object):
 class Parameters(object):
     def __init__(self):
         self.nantennas = 48
+        self.nbeams = 32 * 32
         self.npol = 2
         self.naccumulate = 16
         self.nchannels = 64
@@ -62,15 +62,17 @@ class Parameters(object):
         return self.sample_count() * self.tsamp
 
     def _validate(self):
-        if self.nantennas%2 != 0:
+        if self.nantennas%4 != 0:
             raise ValueError("Number of antennas must be a multiple of 4")
         if 32%self.warp_size != 0:
             raise ValueError("Warp size must divide 32")
+        if self.nbeams%32 !=0:
+            raise ValueError("Number of beams must be a multiple of 32")
         if self.nthreads%self.warp_size !=0:
             raise ValueError("Number of threads must be a multiple of the warp size")
 
     def op_count(self):
-        per_sample = (4*self.nantennas - 1) 
+        per_sample = (8*self.nantennas - 1) * self.nbeams
         ops = per_sample * self.sample_count() * self.npol * self.nchannels
         return ops
         
@@ -82,7 +84,7 @@ class Parameters(object):
         print("#define NWARPS_PER_BLOCK NTHREADS/WARP_SIZE",file=f)
         print("#define NANTENNAS %d"%self.nantennas,file=f)
         print("#define NPOL %d"%self.npol,file=f)
-        print("#define NBEAMS 512",file=f)
+        print("#define NBEAMS %d"%self.nbeams,file=f)
         print("#define NACCUMULATE %d"%self.naccumulate,file=f)
         print("#define NSAMPLES_PER_BLOCK (NACCUMULATE * NTHREADS/WARP_SIZE)",file=f)
         print("#define NSAMPLES (NSAMPLES_PER_BLOCK * %d)"%self.nchunks,file=f)
@@ -144,6 +146,7 @@ def integer_list(string):
 def main(args):
     f = Tee(args.outfile)
     print("#Nants",end="  ",file=f)
+    print("#Nbeams",end="  ",file=f)
     print("#Npol",end="  ",file=f)
     print("#Nchans",end="  ",file=f)
     print("#Naccumulate",end="  ",file=f)
@@ -153,39 +156,42 @@ def main(args):
     print("#Benchmark(s)",end="  ",file=f)
     print("#Performance(Tops/s)",end="  ",file=f)
     print("#Realtime_fraction",end="\n",file=f)
-    for nants in args.nantennas:
-        for npol in args.npol:
-            for nacc in args.naccumulate:
-                for nchan in args.nchannels:
-                    for nchunks in args.nchunks:
-                        for nwarps_per_block in args.nwarps_per_block:
-                            params = Parameters()
-                            params.nantennas = nants
-                            params.npol = npol
-                            params.naccumulate = nacc
-                            params.nchannels = nchan
-                            params.nchunks = nchunks
-                            params.nthreads = nwarps_per_block * 32
-                            params.tsamp = 1/args.channel_bandwidth
-                            params.check_correctness = args.check_correctness
-                            params.niterations = args.niterations
-                            compile(params)
-                            elapsed_time = run()
-                            if elapsed_time is None: raise Exception("No timing output for kernel run.")
-                            seconds_per_run = (elapsed_time*1e-3)/params.niterations
-                            real_time_fraction = seconds_per_run/params.duration()
-                            performance = params.op_count()/seconds_per_run
-                            tops = performance/1e12
-                            print(nants,end="\t",file=f)
-                            print(npol,end="\t",file=f)
-                            print(nchan,end="\t",file=f)
-                            print(nacc,end="\t",file=f)
-                            print(nchunks,end="\t",file=f)
-                            print(params.nthreads,end="\t",file=f)
-                            print("%g"%params.op_count(),end="\t",file=f)
-                            print("%g"%seconds_per_run,end="\t",file=f)
-                            print("%g"%tops,end="\t",file=f)
-                            print("%g"%real_time_fraction,end="\n",file=f)
+    for nbeams in args.nbeams:
+        for nants in args.nantennas:
+            for npol in args.npol:
+                for nacc in args.naccumulate:
+                    for nchan in args.nchannels:
+                        for nchunks in args.nchunks:
+                            for nwarps_per_block in args.nwarps_per_block:
+                                params = Parameters()
+                                params.nbeams = nbeams
+                                params.nantennas = nants
+                                params.npol = npol
+                                params.naccumulate = nacc
+                                params.nchannels = nchan
+                                params.nchunks = nchunks
+                                params.nthreads = nwarps_per_block * 32
+                                params.tsamp = 1/args.channel_bandwidth
+                                params.check_correctness = args.check_correctness
+                                params.niterations = args.niterations
+                                compile(params)
+                                elapsed_time = run()
+                                if elapsed_time is None: raise Exception("No timing output for kernel run.")
+                                seconds_per_run = (elapsed_time*1e-3)/params.niterations
+                                real_time_fraction = seconds_per_run/params.duration()
+                                performance = params.op_count()/seconds_per_run
+                                tops = performance/1e12
+                                print(nants,end="\t",file=f)
+                                print(nbeams,end="\t",file=f)
+                                print(npol,end="\t",file=f)
+                                print(nchan,end="\t",file=f)
+                                print(nacc,end="\t",file=f)
+                                print(nchunks,end="\t",file=f)
+                                print(params.nthreads,end="\t",file=f)
+                                print("%g"%params.op_count(),end="\t",file=f)
+                                print("%g"%seconds_per_run,end="\t",file=f)
+                                print("%g"%tops,end="\t",file=f)
+                                print("%g"%real_time_fraction,end="\n",file=f)
                                 
     
 if __name__ == "__main__":
@@ -193,7 +199,7 @@ if __name__ == "__main__":
     import argparse
 
     msg = """
-    Benchmarking tool for incoherent filterbanking beamformer application.
+    Benchmarking tool for dp4a-based filterbanking beamformer application.
 
     Arguments can be passed as comma separated lists.
     """
@@ -201,6 +207,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage=msg)
     parser.add_argument("--nantennas", type=integer_list, default="32", required=False,
                         help="Number of antennas.")
+    parser.add_argument("--nbeams", type=integer_list, default="512", required=False,
+                        help="Number of beams.")
     parser.add_argument("--npol", type=integer_list, default="2", required=False,
                         help="Number of polarisations.")
     parser.add_argument("--naccumulate", type=integer_list, default="16", required=False,
